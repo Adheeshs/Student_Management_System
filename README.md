cdf859be96e7c650affc4c957fd68b5b2f0ce13a
Adheesh <as.adheesh@outlook.in>
2022-02-14 20:49:20 +0530

    Whole Code is in this file now.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # School_Management_System
-It's a python School Management System created fully in python
+This is a Simple Student Management System Developed for Educational Purpose using Python.
+
+Feel free to make changes based on your requirements.
+
+And if you like this project then ADD a STAR ⭐️  to this project 👆
--- School_Management_System/main.py
+++ School_Management_System/main.py
@@ -1,16 +1,88 @@
-# This is a sample Python script.
+import os
+import platform
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+global listStd #Making ListStd As Super Global Variable
+listStd = ["Adheesh Soni", "Harry Sharma", "Bill Gates", "Steve Jobs", "Elon Musk","Jeff Bezoz"] #List Of Students
 
+def manageStudent(): #Function For The Student Management System
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+	x = "#" * 30
+	y = "=" * 28
+	global bye #Making Bye As Super Global Variable
+	bye = "\n {}\n# {} #\n# ===> Brought To You By <===  #\n# ===> Adheesh Soni <===  #\n# {} #\n {}".format(x, y, y, x) # Will Print GoodBye Message
 
+	#Printing Welcome Message And options For This Program
+	print(""" 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+  ------------------------------------------------------
+ |======================================================| 
+ |======== Welcome To Student Management System	========|
+ |======================================================|
+  ------------------------------------------------------
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+Enter 1 : To View Student's List 
+Enter 2 : To Add New Student 
+Enter 3 : To Search Student 
+Enter 4 : To Remove Student 
+		
+		""")
+
+	try: #Using Exceptions For Validation
+		userInput = int(input("Please Select An Above Option: ")) #Will Take Input From User
+	except ValueError:
+		exit("\nHy! That's Not A Number") #Error Message
+	else:
+		print("\n") #Print New Line
+
+	#Checking Using Option	
+	if(userInput == 1): #This Option Will Print List Of Students
+		print("List Students\n")  
+		for students in listStd:
+			print("=> {}".format(students))
+
+	elif(userInput == 2): #This Option Will Add New Student In The List
+		newStd = input("Enter New Student: ")
+		if(newStd in listStd): #This Condition Checking The New Student Is Already In List Ur Not
+			print("\nThis Student {} Already In The Database".format(newStd))  #Error Message
+		else:	
+			listStd.append(newStd)
+			print("\n=> New Student {} Successfully Add \n".format(newStd))
+			for students in listStd:
+				print("=> {}".format(students))	
+
+	elif(userInput == 3): #This Option Will Search Student From The List
+		srcStd = input("Enter Student Name To Search: ")
+		if(srcStd in listStd): #This Condition Searching The Student
+			print("\n=> Record Found Of Student {}".format(srcStd))
+		else:
+			print("\n=> No Record Found Of Student {}".format(srcStd)) #Error Message
+
+	elif(userInput == 4): #This Option Will Remove Student From The List
+		rmStd = input("Enter Student Name To Remove: ")
+		if(rmStd in listStd): #This Condition Removing The Student From The List 
+			listStd.remove(rmStd)
+			print("\n=> Student {} Successfully Deleted \n".format(rmStd))
+			for students in listStd:
+				print("=> {}".format(students))
+		else:
+			print("\n=> No Record Found of This Student {}".format(rmStd)) #Error Message
+	 
+	elif(userInput < 1 or userInput > 4): #Validating User Option
+		print("Please Enter Valid Option")	#Error Message	
+						
+#brought to you by code-projects.org
+manageStudent()
+
+def runAgain(): #Making Runable Problem1353
+	runAgn = input("\nwant To Run Again Y/n: ")
+	if(runAgn.lower() == 'y'):
+		if(platform.system() == "Windows"): #Checking User OS For Clearing The Screen
+			print(os.system('cls')) 
+		else:
+			print(os.system('clear'))
+		manageStudent()
+		runAgain()
+	else:
+		quit(bye) #Print GoodBye Message And Exit The Program
+
+runAgain()		
